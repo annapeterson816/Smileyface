c4f635bf5daff55d8677a44b1a4e82bdd4ed9065
annapeterson816 <annapeterson816@gmail.com>
2017-04-10 12:58:34 -0500

    Add files via upload

---
+++ face/face.js
@@ -0,0 +1,30 @@
+function setup() {
+
+ createCanvas(1000,1000);
+}
+
+function draw() {
+ // background(255); 
+  
+  fill(255);
+  stroke(25);
+  ellipse(50,50,50,50);
+  
+  fill(255,255,0);
+  stroke(25);
+  ellipse(700,400,300,300);
+  
+  fill(0,0,0);
+  stroke(25);
+  ellipse(760,345,45,45);
+  
+  fill(0,0,0);
+  stroke(25);
+  ellipse(640,345,45,45);
+  
+  fill(0,0,0);
+  stroke(25);
+  ellipse(700,460,85,85);
+  
+  
+}
